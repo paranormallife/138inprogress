86b90daf948c290e453c288a08bb797102a2fd58
elliotshero <elliot@sherodesigns.com>
2019-07-17 17:29:42 -0400

    Homepage contact

--- wp-content/themes/asw2019/index.php
+++ wp-content/themes/asw2019/index.php
@@ -8,4 +8,10 @@
     </div>
 </main>
 
+<?php if( is_home() ) { ?>
+    <section class="homepage-contact">
+        <strong>Contact us:</strong> <a title="Send an email" href="mailto:mariangrudko@138inprogresspublishing.com">mariangrudko@138inprogresspublishing.com</a>
+    </section>
+<?php } ?>
+
 <?php get_footer(); ?>
